c62390c924ddfa0b8296914a747f815577adb929
VinothKumarRajendran <vkrmailbox@gmail.com>
2022-07-01 18:44:52 +0530

    Add project files.

---
+++ ECommerce.Api.Search/Controllers/SearchController.cs
@@ -0,0 +1,31 @@
+﻿using ECommerce.Api.Search.Interfaces;
+using ECommerce.Api.Search.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Search.Controllers
+{
+    [ApiController]
+    [Route("api/search")]
+    public class SearchController : ControllerBase
+    {
+        private readonly ISearchService searchService;
+
+        public SearchController(ISearchService searchService)
+        {
+            this.searchService = searchService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SearchAsync(SearchQuery searchQuery)
+        {
+            var result = await searchService.SearchAsync(searchQuery.CustomerId);
+            if (result.IsSuccess) return Ok(result.SearchResults);
+
+            return NotFound();
+        }
+    }
+}
---
+++ ECommerce.Api.Search/Models/SearchQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Search.Models
+{
+    public class SearchQuery
+    {
+        public int CustomerId { get; set; }
+    }
+}
