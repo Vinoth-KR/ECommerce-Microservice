da20c0f8f42f72b802edb0ccaf3d2e42f20b8334
Vinoth-KR <86417728+Vinoth-KR@users.noreply.github.com>
2022-07-04 00:09:14 +0530

    Updating SearchService to include Orders and Products Service.

--- ECommerce.Api.Search/Services/SearchService.cs
+++ ECommerce.Api.Search/Services/SearchService.cs
@@ -8,10 +8,38 @@ namespace ECommerce.Api.Search.Services
 {
     public class SearchService : ISearchService
     {
+        private readonly IOrdersService orderService;
+        private readonly IProductsService productsService;
+
+        public SearchService(IOrdersService orderService, IProductsService productsService)
+        {
+            this.orderService = orderService;
+            this.productsService = productsService;
+        }
+
         public async Task<(bool IsSuccess, dynamic SearchResults)> SearchAsync(int customerId)
         {
-            await Task.Delay(10);
-            return (true, new { Message = "Hello" });
+            var ordersResult = await orderService.GetOrdersAsync(customerId);
+            var productsResult = await productsService.GetProductsAsync();
+
+            if(ordersResult.IsSuccess)
+            {
+                foreach (var order in ordersResult.Orders)
+                {
+                    foreach (var item in order.Items)
+                    {
+                        item.ProductName = productsResult.IsSuccess ? productsResult.Products.FirstOrDefault(p => p.Id == item.ProductId)?.Name :
+                            "Product Information is not available !";
+                    }
+
+                }
+                var result = new
+                {
+                    Orders = ordersResult.Orders
+                };
+            return (true, result);
+            }
+            return (false, null);
         }
     }
 }
--- ECommerce.Api.Search/Startup.cs
+++ ECommerce.Api.Search/Startup.cs
@@ -8,9 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Polly;
 
 namespace ECommerce.Api.Search
 {
@@ -27,6 +25,16 @@ namespace ECommerce.Api.Search
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<ISearchService, SearchService>();
+            services.AddScoped<IOrdersService, OrdersService>();
+            services.AddScoped<IProductsService, ProductsService>();
+            services.AddHttpClient("OrdersService", config =>
+            {
+                config.BaseAddress = new Uri(Configuration["Services:Orders"]);
+            });
+            services.AddHttpClient("ProductsService", config =>
+            {
+                config.BaseAddress = new Uri(Configuration["Services:Products"]);
+            }).AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(5, _ => TimeSpan.FromMilliseconds(500)));
             services.AddControllers();
         }
 
